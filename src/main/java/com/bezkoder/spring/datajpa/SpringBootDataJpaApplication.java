180e85c7c2bd373fcd2d297d59a16174c9806215
Harouy <harouyoussama@gmail.com>
2023-12-09 14:41:19 +0100

    first

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,12 +2,15 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringBootDataJpaApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
 	}
 
 }
--- src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
@@ -62,4 +62,6 @@ public class Tutorial {
 		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
 	}
 
+
+
 }
